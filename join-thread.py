3f8f85b8fe3e81ecbe8afe9cc531c2cf7fc83148
Jarjanazi <64444222+mrvanonmus@users.noreply.github.com>
2022-10-05 07:22:55 +0200

    Update join-thread.py

--- join-thread.py
+++ join-thread.py
@@ -20,7 +20,6 @@ headers = {
 "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
 "x-requested-with": "xmlhttprequest"
 }
-msg = input("enter msg")
 ndcid = input("enter ndcid")
 threadId = input("enter threadId")
 data = {
